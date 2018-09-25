c09fb529dfda4eb5299d492d64cbebff7e6fd92f
Ivan Badbaden <ivanbadbaden@gmail.com>
2018-09-25 20:40:15 +0800

    added tables

--- src/components/Tables/Ingoing/FrToUro2.vue
+++ src/components/Tables/Ingoing/FrToUro2.vue
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <div class="max300">
-    <md-table v-model="users" :table-header-color="tableHeaderColor">
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="Time Date">{{ item.time_date }}</md-table-cell>
-        <md-table-cell md-label="Tracking No.">{{ item.tracking_number }}</md-table-cell>
-        <md-table-cell md-label="Research Title">{{ item.title }}</md-table-cell>
-        <md-table-cell md-label="Action">
-          <md-button class="md-raised md-success">Receive</md-button>
-        </md-table-cell>
-      </md-table-row>
-    </md-table>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'fr-to-uro2',
-  props: {
-    tableHeaderColor: {
-      type: String,
-      default: ''
-    }
-  },
-  data () {
-    return {
-      selected: [],
-      users: [
-        {
-          trackNo: 1001,
-          ResTitle: 'Dakota Rice',
-          time_date: '10:30:02/02/15/18',
-          transaction: 'Niger'
-        },
-        {
-          trackNo: 1002,
-          ResTitle: 'Dakota Rice',
-          time_date: '10:30:02/02/15/18',
-          transaction: 'Niger'
-        },
-        {
-          trackNo: 1003,
-          ResTitle: 'Dakota Rice',
-          time_date: '10:30:02/02/15/18',
-          transaction: 'Niger'
-        },
-        {
-          trackNo: 1004,
-          ResTitle: 'Dakota Rice',
-          time_date: '10:30:02/02/15/18',
-          transaction: 'Niger'
-        }
-      ]
-    }
-  }
-}
-</script>
-
-<style scoped>
-.max300 {
-  max-height: 500px !important;
-  overflow: scroll;
-}
-</style>
-=======
 <template>
   <div class="max300">
     <md-table v-model="documentsState12" :table-header-color="tableHeaderColor">
@@ -121,4 +55,3 @@ export default {
   overflow: scroll;
 }
 </style>
->>>>>>> 16c1a65dc68a8ccf48e8db9e421cc11dc2764a08
